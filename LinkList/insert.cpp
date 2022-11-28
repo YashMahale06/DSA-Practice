c6c9fe9d79e2975174d5ffd67c68fe293a306f79
YashMahale06 <yashmahale2002@gmail.com>
2022-11-28 15:42:23 +0530

    Insertat head , Insert at Tail,Search in LinkList

---
+++ LinkList/insert.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+
+class  node{
+    public:
+    int data;
+    node* next;
+    node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val)
+{
+    node*n=new node(val);
+    n->next=head;
+    head=n;
+
+}
+
+void insertAtTail(node* &head,int val)
+{
+
+    
+    node *n=new node(val);
+    if(head==NULL)
+    {
+        head=n;
+        return;
+    }
+    node*temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+
+    }
+    temp->next=n;
+
+}
+
+bool Search(node* head,int key){
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        if(temp->data==key){
+            return true;
+        }
+        temp=temp->next;
+    }   
+    return false;
+}
+
+void Display(node* head)
+{
+    node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main()
+{
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    Display(head);
+    insertAtHead(head,4);
+    Display(head);
+    cout<<Search(head,5)<<endl;
+    return 0;
+}
